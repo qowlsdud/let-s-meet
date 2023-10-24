da596d4ea30b41cc0bc6db075676204efa474303
qowls <qowlsdud104@gmail.com>
2023-10-24 10:43:07 +0900

    rmfflsmsrj

--- app/src/main/java/com/example/lets_meet/IntroActivity.kt
+++ app/src/main/java/com/example/lets_meet/IntroActivity.kt
@@ -1,11 +1,20 @@
 package com.example.lets_meet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.lets_meet.onboard.ViewPagerActivity
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        Handler().postDelayed({
+            val intent = Intent(this, ViewPagerActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
