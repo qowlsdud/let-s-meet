ce0dfd1476fdfcdcdb8575833474219a672c2b3a
ryujisung <jiyesung1227@naver.com>
2023-11-06 23:29:37 +0900

    version 0.0.1

--- app/src/main/java/com/example/lets_meet/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/example/lets_meet/ui/signup/SignUpViewModel.kt
@@ -35,7 +35,7 @@ class SignUpViewModel: ViewModel() {
                         "id" to uid.toString(),
                         "email" to email.value.toString(),
                         "name" to nickname.value.toString(),
-                        "profileImageUrl" to "https://mblogthumb-phinf.pstatic.net/MjAyMDExMDFfMTgy/MDAxNjA0MjI4ODc1NDMw.Ex906Mv9nnPEZGCh4SREknadZvzMO8LyDzGOHMKPdwAg.ZAmE6pU5lhEdeOUsPdxg8-gOuZrq_ipJ5VhqaViubI4g.JPEG.gambasg/%EC%9C%A0%ED%8A%9C%EB%B8%8C_%EA%B8%B0%EB%B3%B8%ED%94%84%EB%A1%9C%ED%95%84_%ED%95%98%EB%8A%98%EC%83%89.jpg?type=w800",
+                        "profileImageUrl" to "https://i.namu.wiki/i/Bge3xnYd4kRe_IKbm2uqxlhQJij2SngwNssjpjaOyOqoRhQlNwLrR2ZiK-JWJ2b99RGcSxDaZ2UCI7fiv4IDDQ.webp",
                         "state" to "없음"
                     )
                     db.collection(email.value.toString())
