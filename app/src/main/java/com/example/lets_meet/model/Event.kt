30494a64fda9e34306f303011cd7503418985efc
ryujisung <jiyesung1227@naver.com>
2023-11-11 13:53:38 +0900

    version 0.0.1

--- app/src/main/java/com/example/lets_meet/model/Event.kt
+++ app/src/main/java/com/example/lets_meet/model/Event.kt
@@ -3,7 +3,7 @@ package com.example.lets_meet.model
 data class Event(
     var date: String = "",
     var title: String ="",
-
+    var notice: Boolean = false,
     var starttime: String="",
     var endtime: String = "",
     var public: Boolean = false
